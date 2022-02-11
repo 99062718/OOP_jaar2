fa1b6fdf06ec0429aac2a2e880a978d10aa9a793
tycho <99062718@mydavinci.nl>
2022-02-11 13:11:03 +0100

    created function pokeAttack() and fixed a few bugs

--- pokebattle.py
+++ pokebattle.py
@@ -1,3 +1,5 @@
+pokemons = {}
+
 class pokemon:
     def __init__(self, name, energyType, hitpoints, attacks, resistances, weaknesses):
         self.__pokeInfo = {
@@ -7,7 +9,7 @@ class pokemon:
             "health": hitpoints,
             "attacks": attacks,
             "resistances": resistances,
-            "weakenesses": weaknesses
+            "weaknesses": weaknesses
         }
 
     def getStat(self, stat):
@@ -33,14 +35,25 @@ class pokemon:
 
         for resistance in self.getStat("resistances"):
             if resistance[0] == energyType:
-                damage /= weakness[1]
+                damage /= resistance[1]
                 break
 
         self.appendOrChangeStat("subtract", "health", round(damage, 0))
 
         if self.__pokeInfo["health"] <= 0:
+            self.__pokeInfo["health"] == 0
             print("{} has fainted!".format(self.getStat("name")))
 
-pikachu = pokemon("pikachu", "lightning", 60, {"electric ring": 50, "pika punch": 20}, [["fighting"], 2], [["fire", 1.5]])
 
-charmeleon = pokemon("charmeleon", "fire", 60, {"headbutt": 10, "flare": 30}, [["lightning", 10]], [["water", 2]])
+def pokeAttack(nameAttacker, nameAttacked, attack):
+    print(f"{nameAttacked} has {pokemons[nameAttacked].getStat('health')} hp")
+    print(f"{nameAttacker} uses {attack[0]} on {nameAttacked}")
+    pokemons[nameAttacked].takeDamage(attack[1], pokemons[nameAttacker].getStat("energyType"))
+    print(f"{nameAttacked} has {pokemons[nameAttacked].getStat('health')} hp")
+
+pokemons["pikachu"] = pokemon("pikachu", "lightning", 60, {"electric ring": 50, "pika punch": 20}, [["fighting", 2]], [["fire", 1.5]])
+
+pokemons["charmeleon"] = pokemon("charmeleon", "fire", 60, {"headbutt": 10, "flare": 30}, [["lightning", 10]], [["water", 2]])
+
+pokeAttack("pikachu", "charmeleon", list(pokemons["pikachu"].getStat("attacks").items())[0])
+pokeAttack("charmeleon", "pikachu", list(pokemons["charmeleon"].getStat("attacks").items())[1])
