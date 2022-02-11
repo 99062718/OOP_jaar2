e7b879d41b9e5be0bc0e5c2c76fe8a9c44f5c37c
tycho <99062718@mydavinci.nl>
2022-02-11 11:23:07 +0100

    added __init__ to class superhero

--- superhero.py
+++ superhero.py
@@ -1,2 +1,6 @@
 class superhero:
-    pass
+    def __init__(self, name, gender, team, oneliner):
+        self.name = name
+        self.gender = gender
+        self.team = team
+        self.oneliner = oneliner
