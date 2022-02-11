96e0f91c6e4a829f1c81bedcb883811d179d8d33
tycho <99062718@mydavinci.nl>
2022-02-11 11:49:09 +0100

    added function sayOneliner() to class superhero

--- superhero.py
+++ superhero.py
@@ -5,4 +5,9 @@ class superhero:
         self.team = team
         self.oneliner = oneliner
 
+    def sayOneliner(self):
+        print(self.oneliner)
+
 spiderman =  superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!')
+
+spiderman.sayOneliner()
